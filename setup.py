e898906edfd5bf285c7408893d2d54f60afedfb1
Jeremy Low <jeremylow@gmail.com>
2015-10-22 12:48:10 -0400

    init for building

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='django-dri',
+    packages=['django-dri'],
+    version='0.0.1',
+    author='Jeremy Low',
+    url='https://github.com/jeremylow/dri',
+    install_requires=['django'],
+    license='MIT'
+    )
